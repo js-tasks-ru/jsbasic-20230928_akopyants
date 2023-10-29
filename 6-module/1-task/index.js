3428ec459bdd58d230af4e4a1d6e6ce008961404
Akopyants <akopyants92@yandex.ru>
2023-10-29 13:26:57 +0300

    6-module

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,52 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.createTable();
   }
+
+  createTable() {
+    const table = document.createElement("table");
+    table.appendChild(this.createThead());
+    table.appendChild(this.createBody());
+    table.addEventListener("click", this.onClick);
+    return table;
+  }
+
+  createThead() {
+    const thead = document.createElement("thead");
+    const tr = document.createElement("tr");
+    const names = ["Имя", "Возраст", "Зарплата", "Город", ""];
+
+    tr.innerHTML = names.map((item) => `<th>${item}</th>`).join("");
+
+    thead.appendChild(tr);
+
+    return thead;
+  }
+
+  createBody() {
+    const tBody = document.createElement("tbody");
+
+    this.rows.forEach((item) => {
+      const tr = document.createElement("tr");
+      const values = Object.values(item);
+
+      tr.innerHTML = values.map((item) => `<td>${item}</td>`).join("");
+
+      const deleteButton = document.createElement("td");
+      deleteButton.innerHTML = "<button>x</button>";
+      tr.appendChild(deleteButton);
+
+      tBody.appendChild(tr);
+    });
+
+    return tBody;
+  }
+
+  onClick = (event) => {
+    if (event.target.tagName === "BUTTON") {
+      const tr = event.target.closest("tr");
+      tr.remove();
+    }
+  };
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,98 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.index = 0;
+    this.sliderWidht = 0;
+    this.elem = this.createCarousel();
+    this.elem.addEventListener("click", this.onClick);
+  }
+
+  createCarousel() {
+    const carousel = document.createElement("div");
+    carousel.className = "carousel";
+    carousel.appendChild(this.createSlides());
+    this.createNavigation().forEach((item) => carousel.appendChild(item));
+
+    return carousel;
+  }
+
+  createSlides() {
+    let carouselInner = document.createElement("div");
+    carouselInner.className = "carousel__inner";
+
+    this.slides.forEach((element) => {
+      const slide = createElement(`
+      <div class="carousel__slide" data-id="${element.id}">
+        <img src="/assets/images/carousel/${element.image}" class="carousel__img" alt="slide">
+        <div class="carousel__caption">
+          <span class="carousel__price">€${element.price.toFixed(2)}</span>
+          <div class="carousel__title">${element.name}</div>
+          <button type="button" class="carousel__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+      `);
+      carouselInner.appendChild(slide);
+    });
+
+    this.carouselInnerWidth = carouselInner.offsetWidth;
+    return carouselInner;
+  }
+
+  createNavigation() {
+    let left = createElement(`
+      <div class="carousel__arrow carousel__arrow_left" style="display: none">
+        <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+      </div>
+    `);
+
+    let right = createElement(`
+      <div class="carousel__arrow carousel__arrow_right">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </div>
+    `);
+
+    return [left, right];
+  }
+
+  switchSlide = (el) => {
+    const step = this.sliderWidht * -this.index;
+    el.style.transform = `translateX(${step}px)`;
+  };
+
+  onClick = (event) => {
+    const left = this.elem.querySelector(".carousel__arrow_left");
+    const right = this.elem.querySelector(".carousel__arrow_right");
+    const carouselInner = this.elem.querySelector(".carousel__inner");
+    this.sliderWidht = carouselInner.offsetWidth;
+
+    if (event.target.closest(".carousel__arrow_left")) {
+      this.index--;
+    }
+
+    if (event.target.closest(".carousel__arrow_right")) {
+      this.index++;
+    }
+
+    if (event.target.closest(".carousel__button")) {
+      const id = event.target.closest(".carousel__slide").dataset.id;
+      const addProduct = new CustomEvent("product-add", {
+        detail: id,
+        bubbles: true,
+      });
+
+      this.elem.dispatchEvent(addProduct);
+    }
+
+    this.switchSlide(carouselInner);
+    this.buttonState(left, right);
+  };
+
+  buttonState(left, right) {
+    left.style.display = this.index === 0 ? "none" : "";
+    right.style.display = this.index === this.slides.length - 1 ? "none" : "";
   }
 }

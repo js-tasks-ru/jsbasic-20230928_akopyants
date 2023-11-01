cc12c19435d5298b374de64b9e5a6973538f29bf
Akopyants <akopyants92@yandex.ru>
2023-11-01 23:11:47 +0300

    7-module

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,7 +1,91 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    this.nav = this.createNav();
+    this.nav.addEventListener('scroll', this.onScroll);
+    this.navPrev = this.createNavButton("prev");
+    this.navNext = this.createNavButton("next");
+    this.elem = this.createMenu();
+  }
+
+  createMenu() {
+    const menu = document.createElement("div");
+    menu.className = "ribbon";
+    menu.append(this.navPrev, this.nav, this.navNext);
+    menu.addEventListener("click", this.onClick);
+    
+    return menu;
+  }
+
+  createNav() {
+    const nav = document.createElement("nav");
+    nav.className = "ribbon__inner";
+    nav.innerHTML = this.categories.map(element => `
+      <a href="#" class="ribbon__item" data-id="${element.id}">${element.name}</a>
+    `).join("");
+
+    return nav;
+  }
+
+  createNavButton(direction) {
+    const arrow = document.createElement("button");
+    const imgPath = "/assets/images/icons/angle-icon.svg";
+    const directionClass = `ribbon__arrow ${
+      direction === "prev" ? "ribbon__arrow_left" : "ribbon__arrow_right ribbon__arrow_visible"
+    }`;
+    arrow.className = directionClass;
+    arrow.innerHTML = `<img src="${imgPath}" alt="icon">`;
+    
+    return arrow;
+  }
+
+  onClick = event => {
+    const prevButton = event.target.closest(".ribbon__arrow_left");
+    const nextButton = event.target.closest(".ribbon__arrow_right");
+    const ribbonItem = event.target.closest(".ribbon__item");
+
+    if (prevButton) {
+      this.nav.scrollBy(-350, 0);
+    }
+
+    if (nextButton) {
+      this.nav.scrollBy(350, 0);
+    }
+
+    if (ribbonItem) {
+      event.preventDefault();
+      this.handleRibbonItemClick(ribbonItem);
+    }
+  };
+
+  handleRibbonItemClick(ribbonItem) {
+    const links = this.nav.querySelectorAll(".ribbon__item");
+    const id = ribbonItem.dataset.id;
+
+    links.forEach(element => element.classList.remove("ribbon__item_active"));
+    ribbonItem.classList.add("ribbon__item_active");
+
+    const ribbonSelect = new CustomEvent("ribbon-select", {
+      detail: id,
+      bubbles: true,
+    });
+
+    this.elem.dispatchEvent(ribbonSelect);
+  }
+
+  hideNav() {
+    const scrollWidth = this.nav.scrollWidth;
+    const scrollLeft = this.nav.scrollLeft;
+    const clientWidth = this.nav.clientWidth;
+    const scrollRight = scrollWidth - scrollLeft - clientWidth;
+
+    this.navPrev.classList.toggle('ribbon__arrow_visible', this.nav.scrollLeft !== 0);
+    this.navNext.classList.toggle('ribbon__arrow_visible', scrollRight >= 1);
+  }
+
+  onScroll = () => {
+    this.hideNav();
   }
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,65 @@
+import createElement from "../../assets/lib/create-element.js";
+
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.steps = steps;
+    this.value = value;
+    this.elem = this.createStepSlider();
+    this.thumb = this.elem.querySelector(".slider__thumb");
+    this.progress = this.elem.querySelector(".slider__progress");
+    this.sliderValues = this.elem.querySelector(".slider__value");
+    this.elem.addEventListener("click", this.onClick);
+  }
+
+  createStepSlider() {
+    const slider = createElement(`
+      <div class="slider">
+        <div class="slider__thumb">
+          <span class="slider__value">0</span>
+        </div>
+    
+        <div class="slider__progress"></div>
+    
+        <div class="slider__steps">
+          <span class="slider__step-active"></span>
+          ${"<span></span>".repeat(this.steps - 1)}
+        </div>
+      </div>`);
+
+    return slider;
+  }
+
+  calculateValue(leftRelative) {
+    const segments = this.steps - 1;
+    const approximateValue = leftRelative * segments;
+    return Math.round(approximateValue);
+  }
+
+  updateSlider(value) {
+    const valuePercents = (value / (this.steps - 1)) * 100;
+    this.thumb.style.left = `${valuePercents}%`;
+    this.sliderValues.textContent = value;
+    this.progress.style.width = `${valuePercents}%`;
+  }
+
+  onClick = (event) => {
+    const left = event.clientX - this.elem.getBoundingClientRect().left;
+    const leftRelative = left / this.elem.offsetWidth;
+    const value = this.calculateValue(leftRelative);
+
+    this.updateSlider(value);
+
+    this.value = value;
+
+    this.changeEvent(value);
+  };
+
+  changeEvent(value) {
+    const sliderChange = new CustomEvent("slider-change", {
+      detail: this.value,
+      bubbles: true,
+    });
+
+    this.elem.dispatchEvent(sliderChange);
   }
 }
